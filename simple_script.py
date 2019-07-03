3f812797d3e3af5fad853ac1925d7cb80e3ee2d4
Lino Galiana <lino.galiana@insee.fr>
2019-07-03 09:34:15 +0200

    Changing execution timing

--- simple_script.py
+++ simple_script.py
@@ -1,17 +1,18 @@
 import numpy as np
-import time
+import datetime
 import statistics
 
 def test(n = 1000): 
-  start = time.time()
+  start = datetime.datetime.now()
   x = np.random.uniform(0,1,n)
   for i in range(1,n):
     x[i] += x[i-1]
-  end = time.time()
-  return(end - start)
+  end = datetime.datetime.now()
+  c = end - start
+  return(c.microseconds)
 
 exec_time_python = [test(100000) for k in range(100)]
 
-print("Time for a Fibonacci series over a vector of size 1000:")
-print(statistics.median(exec_time_python*1000))
+print("Time for a Fibonacci series over a vector of size 1000 (milliseconds):")
+print(statistics.median(exec_time_python))
 
