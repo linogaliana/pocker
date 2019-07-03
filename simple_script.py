aa88dfde2ed7eaa7f7f03a075ffec4dc898f5a6d
Lino Galiana <lino.galiana@insee.fr>
2019-07-03 09:43:03 +0200

    Setting result in milliseconds

--- simple_script.py
+++ simple_script.py
@@ -9,7 +9,7 @@ def test(n = 1000):
     x[i] += x[i-1]
   end = datetime.datetime.now()
   c = end - start
-  return(c.microseconds)
+  return(c.microseconds/1000)
 
 exec_time_python = [test(100000) for k in range(100)]
 
